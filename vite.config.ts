b4a4add9f6641483adbe9bd85d41332f66a72b53
Lebo-Kharafu <kharafu23@gmail.com>
2026-01-28 13:41:20 +0200

    feat: added datahub correctly

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+// @ts-ignore
+import DataHub from 'macaca-datahub'
+
+const datahubConfig = { 
+  port: 5678, hostname: '127.0.0.1', 
+  store: path.join(__dirname, '..', 'data'), 
+  proxy: { '/api': { hub: 'sample', }, }, 
+  showBoard: true, 
+}
+const defaultDatahub = new DataHub({ port: datahubConfig.port, })
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    { name: 'vite-plugin-datahub', configureServer() { 
+      defaultDatahub.startServer(datahubConfig).then(() => { console.log('datahub ready') }) 
+    }, },
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: { proxy: { '/api': { 
+    target: `http://${datahubConfig.hostname}:${datahubConfig.port}`, 
+    changeOrigin: true, }, }, },
 })
