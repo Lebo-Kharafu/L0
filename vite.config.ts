cea53ef567360f0435b6d1d0396a65f8ab92c7e0
Lebo-Kharafu <kharafu23@gmail.com>
2026-01-29 07:34:28 +0200

    feat: added datahub locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'path'
 // @ts-ignore
 import DataHub from 'macaca-datahub'
+import path from 'node:path'
 
-const datahubConfig = { 
-  port: 5678, hostname: '127.0.0.1', 
-  store: path.join(__dirname, '..', 'data'), 
-  proxy: { '/api': { hub: 'sample', }, }, 
-  showBoard: true, 
+const datahubConfig = {
+  port: 5678, hostname: '127.0.0.1',
+  store: './datahub',
+  proxy: { '/api': { hub: 'letsdo', }, },
+  showBoard: true,
 }
+
 const defaultDatahub = new DataHub({ port: datahubConfig.port, })
 
 // https://vite.dev/config/
@@ -20,16 +20,23 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    { name: 'vite-plugin-datahub', configureServer() { 
-      defaultDatahub.startServer(datahubConfig).then(() => { console.log('datahub ready') }) 
-    }, },
+    {
+      name: 'vite-plugin-datahub', configureServer() {
+        defaultDatahub.startServer(datahubConfig).then(() => { console.log('datahub ready') })
+      },
+    },
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: { proxy: { '/api': { 
-    target: `http://${datahubConfig.hostname}:${datahubConfig.port}`, 
-    changeOrigin: true, }, }, },
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://${datahubConfig.hostname}:${datahubConfig.port}`,
+        changeOrigin: true,
+      },
+    },
+  },
 })
