2ed8409836371573c050089cc262560b9340c772
Lebo-Kharafu <Kharafu23@gmail.com>
2026-02-02 09:04:37 +0200

    feat: new loading and error screen

---
+++ src/views/LoadingView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="loading">
+        {{ "loading" }}
+    </div>
+</template>
+
+<style scoped>
+    #loading{
+        background-color: firebrick;
+        color: aquamarine;
+
+        display: flex;
+
+        min-height: 100px;
+        max-height: 250px;
+        width: 100%;
+
+        justify-content: space-around;
+        align-items: center;
+        text-align: center;
+
+        font-weight: 600;
+        font-size: 6rem;
+    }
+</style>
