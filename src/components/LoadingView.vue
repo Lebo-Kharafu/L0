68a6df36dcc02bcb58b12e8b8c254e205d907015
Lebo-Kharafu <Kharafu23@gmail.com>
2026-02-02 13:35:51 +0200

    hotfix: moved files

---
+++ src/components/LoadingView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="loading">
+        {{ "loading" }}
+    </div>
+</template>
+
+<style scoped>
+    #loading{
+        color: aquamarine;
+
+        display: flex;
+
+        min-height: 100px;
+        max-height: 250px;
+        width: 100%;
+
+        justify-content: space-around;
+        align-items: center;
+        text-align: center;
+
+        font-weight: 600;
+        font-size: 6rem;
+    }
+</style>
