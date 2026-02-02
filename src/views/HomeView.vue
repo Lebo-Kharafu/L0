6e622d6fe74f4617230428585a21d331a85d6d0d
Lebo-Kharafu <Kharafu23@gmail.com>
2026-02-02 07:45:15 +0200

    feat: made home use store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+    import { computed, onMounted } from 'vue';
     import TaskList from '@/components/TaskList.vue';
     import { useTaskStore } from '@/stores/tasks/tasks';
-    const store = useTaskStore();
-    store.getAll();
 
+    const store = useTaskStore();
+    
+    onMounted(() => {
+        store.getAll();
+    });
+
+    const tasks = computed(() => store.state.tasks);
 </script>
 
 <template>
-    <TaskList v-if="store.state.tasks" :arr="store.state.tasks" />
+    <div v-if="store.state.error">Error...</div>
+    <div v-else-if="store.state.isLoading">Loading...</div>
+    <TaskList v-else-if="tasks" :arr="tasks" />
 </template>
 
 <style scoped></style>
