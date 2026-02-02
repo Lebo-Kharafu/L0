2b6c7edefb6c98bd670aadb68e5dc0b59cf8d58d
Lebo-Kharafu <Kharafu23@gmail.com>
2026-02-02 23:50:35 +0200

    feat: changed the router files

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/test',
-      name: 'test',
-      component: () => import('../views/testView.vue'),
-    },
-    {
       path: '/',
       name: 'home',
       component: HomeView,
